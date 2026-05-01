65cc8e8cee852fc9c7052f8a23e95cd1f0a0389e
sun <sunny@sunnymorgan.com>
2026-05-01 17:44:52 -0400

    Load FluentForms fixes after plugins_loaded

--- flashblocks-accessibility.php
+++ flashblocks-accessibility.php
@@ -42,12 +42,18 @@ require_once DIR . '/includes/class-ada-fixes.php';
 new Includes\ADA_Fixes();
 
 // FluentForms-specific fixes (only if plugin is active)
-add_action('plugins_loaded', function () {
-    if (defined('FLUENTFORM')) {
-        require_once DIR . '/includes/class-fluentforms-fixes.php';
-        new Includes\FluentForms_Fixes();
-    }
-});
+function load_fluentforms_fixes(): void {
+	if (defined('FLUENTFORM')) {
+		require_once DIR . '/includes/class-fluentforms-fixes.php';
+		new Includes\FluentForms_Fixes();
+	}
+}
+
+if (did_action('plugins_loaded')) {
+	load_fluentforms_fixes();
+} else {
+	add_action('plugins_loaded', __NAMESPACE__ . '\load_fluentforms_fixes');
+}
 
 if (is_admin()) {
     require_once DIR . '/includes/class-updater.php';
